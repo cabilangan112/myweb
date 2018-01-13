b2ee2fb29b53c760df131857dde54ce910d0c31f
cabilangan112 <jassencabilangan@gmail.com>
2018-01-13 11:26:41 +0800

    start

--- project/models.py
+++ project/models.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.conf import settings
+from django.db import models
+from django.db.models.signals import pre_save
+from django.core.urlresolvers import reverse
+from locations.utils import unique_slug_generator
+from locations.models import Location
+User = settings.AUTH_USER_MODEL
 
-# Create your models here.
+
+class Item(models.Model):
+	User					= models.ForeignKey(User)
+	Location				= models.ForeignKey(Location)
+	Category
+	Item_name				= models.CharField(max_length=120)
+	
