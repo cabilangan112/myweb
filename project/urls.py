318943ba7131d712ab88eec332bcf561732bdd15
cabilangan112 <jassencabilangan@gmail.com>
2018-01-14 22:29:10 +0800

    utls,Views

---
+++ project/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from django.utils import timezone
+
+from .views import (
+	ItemLostView,
+
+	)
+
+
+ 
+
+
+urlpatterns = [
+
+		
+	
+]
--- project/views.py
+++ project/views.py
@@ -1,6 +1,35 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+
+
+from django.views import generic
 from django.shortcuts import render
+from models import ItemLost, OwnerInfo,LocationLost,ReturnerInfo
+from django.views.generic import (ListView,DetailView,CreateView,UpdateView)
 
 # Create your views here.
+class HomeView(ListView):
+	def get(self, request, *args, **kwargs):
+		if not request.user.is_authenticated():
+			return render(request, "index", {})
+		qs = ItemLost.objects.filter(Returned=False).order_by("-updated")[:10]
+		return render(request, " ", {'object_list':qs})
+
+		
+class ReturnedView(ListView):
+	def get(self, request, *args, **kwargs):
+		if not request.user.is_authenticated():
+			return render(request, "home.html", {})
+		qs = ItemLost.objects.filter(Returned=True).order_by("-updated")[:10]
+		return render(request, " ", {'object_list':qs})
+		
+		
+class ItemLostView(generic.ListView):
+	def get(self, request): 
+		items = ItemLost.objects.all()
+		context = {
+			'items':items,
+		
+		}
+		return render(request, "list_lost.html", context)
